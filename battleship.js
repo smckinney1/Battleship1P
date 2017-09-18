0e3f20f78f13cea6d552f543e85710aa6c7bb9d0
Shannon <13708207+smckinney1@users.noreply.github.com>
2017-09-18 11:07:44 -0400

    Added some behavior to the view object to display message, hit, or miss in DOM

--- battleship.js
+++ battleship.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+//Creating a rudimentary MVC-type configuration using vanilla JS
+
+var view = {
+	displayMessage: function (msg) {
+		//display message on screen
+		var messageArea = document.getElementById("message-area");
+		messageArea.innerHTML = msg;
+	},
+	displayHit: function (location) {
+		//change class of table cell to "hit"
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "hit");
+	},
+	displayMiss: function (location) {
+		//change class of table cell to "miss"
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "miss");
+	}
+};
